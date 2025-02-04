453032312a5947c2065765e008dcd80df24eec67
amirali <amirali.mosolo0@gmail.com>
2025-02-04 23:40:11 +0330

    test: test and run Extension"

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,26 @@
 let form = document.querySelector("form");
-let submit = document.querySelector("button")
+let submit = document.querySelector("button");
 
-let loadinPage = () => {
-    document.querySelector("html").classList.add("exiteLoaded");
-} 
+let loadingPage = () => {
+  document.querySelector("html").classList.add("exitLoaded");
+  // TODO
+};
 
 let LoginFrom = () => {
-    if (form == true) {
-        console.log("شما با موفقست ثبت نام شدید");
-        return true;
-    } else {
-        console.log("ثبت نام شما به مشکل خورد :((");
-        return false;
-    }
-}
+  if (form == true) {
+    console.log("شما با موفقست ثبت نام شدید");
+    return true;
+  } else {
+    console.log("ثبت نام شما به مشکل خورد :((");
+    return false;
+  }
+};
 console.log("show modal form");
 // modal must be show
 
-submit.addEventListener("click", LoginFrom)
+submit.addEventListener("click", LoginFrom);
+if (form == true) {
+  console.log("log IN✅")
+} else{
+  // FIXME
+}
