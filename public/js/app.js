1c795674d6531951a447a90009dcfcc90c810779
amirali <amirali.mosolo0@gmail.com>
2025-02-04 20:45:28 +0330

    fixedLoading

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,10 @@
 let form = document.querySelector("form");
 let submit = document.querySelector("button")
 
+let loadinPage = () => {
+    document.querySelector("html").classList.add("exiteLoaded");
+} 
+
 let LoginFrom = () => {
     if (form == true) {
         console.log("شما با موفقست ثبت نام شدید");
