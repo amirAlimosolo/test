4d8364b7224ee70875533e6ffc243dc17fb14302
amirali <amirali.mosolo0@gmail.com>
2025-02-04 20:36:35 +0330

    createFormLoginSumit

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-alert("you must accept alert");
+let form = document.querySelector("form");
+let submit = document.querySelector("button")
+
+let LoginFrom = () => {
+    if (form == true) {
+        console.log("شما با موفقست ثبت نام شدید");
+    } else {
+        console.log("ثبت نام شما به مشکل خورد :((");
+    }
+}
+
+submit.addEventListener("click", LoginFrom)
