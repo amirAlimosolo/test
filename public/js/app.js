7ce2cb26b550daacd333cff30e6539b82af9a076
amirali <amirali.mosolo0@gmail.com>
2025-02-07 23:20:19 +0330

    add: add reset files in repository

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,30 @@
 let form = document.querySelector("form");
-let submit = document.querySelector("button")
+let submit = document.querySelector("button");
+
+let loadingPage = () => {
+  document.querySelector("html").classList.add("exitLoaded");
+  // TODO
+};
 
 let LoginFrom = () => {
-    if (form == true) {
-        console.log("شما با موفقست ثبت نام شدید");
-        return true;
-    } else {
-        console.log("ثبت نام شما به مشکل خورد :((");
-        return false;
-    }
+  if (form == true) {
+    console.log("شما با موفقست ثبت نام شدید");
+    return true;
+  } else {
+    console.log("ثبت نام شما به مشکل خورد :((");
+    return false;
+  }
+};
+console.log("show modal form");
+// modal must be show
+
+submit.addEventListener("click", LoginFrom);
+if (form == true) {
+  console.log("log IN✅");
+} else {
+  // FIXME
 }
 
-submit.addEventListener("click", LoginFrom)
+let sumFunc = (firstNum, secondNum) => {
+  return firstNum + secondNum;
+};
